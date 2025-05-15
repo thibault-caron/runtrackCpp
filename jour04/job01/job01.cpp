9db8be6c0dbf0106dcae6c090f948e0681ea7c94
Thibault Caron <thibault.caron@laplateforme.io>
2025-05-15 10:21:31 +0200

    Feat: Jour04/job01w

---
+++ jour04/job01/job01.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+    // Création de la variable "number" contenant 2019
+    int number = 2019;
+    
+    // Création d'un pointeur vers cette variable
+    int *ptr_number = &number;
+    
+    // Affichage de la valeur de "number" via le pointeur
+    std::cout << "*ptr_number - Valeur de number via le pointeur (dereference): " << *ptr_number << std::endl;
+    
+    // Pour démontrer que le pointeur pointe bien vers la variable "number"
+    std::cout << "number - Valeur directe de number: " << number << std::endl;
+    std::cout << "&number - Adresse de number: " << &number << std::endl;
+    std::cout << "ptr_number - Valeur stockee dans le pointeur (adresse, reference): " << ptr_number << std::endl;
+    
+    return 0;
+}
